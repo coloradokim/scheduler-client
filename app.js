240b68be73a4163648fee1ca97a1abc0ec1f58d2
coloradokim <schlesinger.kim@gmail.com>
2017-12-19 08:56:12 -0700

    add placeholders

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('HIIIII!');
+const API_URL = 'https://mysterious-dawn-91753.herokuapp.com/';
+const LOCAL_URL = 'http://localhost:3000';
+
+function getData() {
+fetch(API_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    localStorage.setItem("data", JSON.stringify(response.data));
+  })
+  .catch(function(error) {
+    console.log(error.message);
+  });
+}
+
+function createText() {
+  let div = document.querySelector('.hobbies');
+  let newParagraph = document.createElement('p');
+  let newText = document.createTextNode(localStorage.getItem('data'));
+  newParagraph.append(newText);
+  div.append(newParagraph);
+}
+document.onload = getData()
