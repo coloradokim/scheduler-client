65e4c5c3cc6141e5f4b7e64f7d85792281bf363c
coloradokim <schlesinger.kim@gmail.com>
2017-12-23 19:16:38 -0700

    add logic to make ajax call only if nothing is in localStorage

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const API_URL = 'https://mysterious-dawn-91753.herokuapp.com/';
 const LOCAL_URL = 'http://localhost:3000';
 
 function getData() {
-fetch(API_URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    localStorage.setItem("data", JSON.stringify(response.data));
-  })
-  .catch(function(error) {
-    console.log(error.message);
-  });
+  if (localStorage.getItem('data')) {
+    console.log('no need for an ajax call');
+  } else {
+  fetch(API_URL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      localStorage.setItem("data", JSON.stringify(response.data));
+    })
+    .catch(function(error) {
+      console.log(error.message);
+    });
+  }
 }
+
 
 function createText() {
   let div = document.querySelector('.hobbies');
@@ -22,3 +27,7 @@ function createText() {
   div.append(newParagraph);
 }
 document.onload = getData()
+
+function submitUserInput() {
+  console.log('hi!');
+}
