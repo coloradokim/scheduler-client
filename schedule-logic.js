73ba28637b14d93fad18eeec94cfa7b41680d0b3
coloradokim <schlesinger.kim@gmail.com>
2017-12-24 21:52:21 -0700

    update logic

--- schedule-logic.js
+++ schedule-logic.js
@@ -6,13 +6,20 @@ var time = document.querySelector('select');
 function getUserInput() {
   date = date.value;
   time = time.value;
-  console.log(time);
-  console.log(addMinTime(hobbyData, time));
+  console.log(createActivityList(hobbyData, time));
 }
 
-function addMinTime(hobbyArray, time) {
-  return hobbyArray.reduce((total, amount, index, array) => {
-    total += array[index].minTime;
-    return total;
-  }, 0);
+function createActivityList(hobbyArray, time) {
+  let totalTime = 0;
+  let activities = [];
+  for (var i = 0; i < hobbyArray.length; i++) {
+    if (totalTime <= time) {
+      totalTime += hobbyArray[i].minTime;
+      activities.push(hobbyArray[i]);
+    } else {
+      break;
+    }
+  }
+  activities.pop();
+  return activities;
 }
