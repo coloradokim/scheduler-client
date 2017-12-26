c4597556c3fa481c311ba18580ad2e0a7d60ba56
coloradokim <schlesinger.kim@gmail.com>
2017-12-26 15:33:20 -0700

    updating js files

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const API_URL = 'https://mysterious-dawn-91753.herokuapp.com/';
-const LOCAL_URL = 'http://localhost:3000';
 
-document.onload = getData();
+document.body.onload = getData();
+
+var date = document.querySelector('input');
+var time = document.querySelector('select');
 
 function getData() {
   if (localStorage.getItem('data')) {
@@ -23,4 +25,48 @@ function getData() {
 function reset() {
   date.value = '';
   time.value = '';
+}
+
+var submitButton = document.getElementsByClassName('button is-link')[0];
+
+submitButton.addEventListener('click', getUserInput, false);
+
+function getUserInput(e) {
+  e.preventDefault();
+  date = date.value;
+  time = time.value;
+  window.location.href = "schedule.html";
+  showActivities()
+}
+
+const hobbyData = JSON.parse(localStorage.getItem('data'));
+
+const shuffleArray = arr => arr.sort(() => Math.random() - 0.5)
+
+var shuffledArray = shuffleArray(hobbyData)
+
+var columnDiv = document.getElementsByClassName('column');
+
+function createActivityList(shuffledArray, time) {
+  let totalTime = 0;
+  let activities = [];
+  for (var i = 0; i < hobbyArray.length; i++) {
+    if (totalTime <= time) {
+      totalTime += hobbyArray[i].minTime;
+      activities.push(hobbyArray[i]);
+    } else {
+      break;
+    }
+  }
+  activities.pop();
+  return activities;
+}
+
+function showActivities() {
+  let activityList = createActivityList(hobbyData, time);
+  var newSection = document.createElement("section");
+  var newContent = document.createTextNode('hi');
+  newSection.appendChild(newContent);
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newSection, currentDiv);
 }
--- schedule-logic.js
+++ schedule-logic.js
@@ -1,25 +1,31 @@
-const hobbyData = JSON.parse(localStorage.getItem('data'));
-
-var date = document.querySelector('input');
-var time = document.querySelector('select');
-
-function getUserInput() {
-  date = date.value;
-  time = time.value;
-  console.log(createActivityList(hobbyData, time));
-}
-
-function createActivityList(hobbyArray, time) {
-  let totalTime = 0;
-  let activities = [];
-  for (var i = 0; i < hobbyArray.length; i++) {
-    if (totalTime <= time) {
-      totalTime += hobbyArray[i].minTime;
-      activities.push(hobbyArray[i]);
-    } else {
-      break;
-    }
-  }
-  activities.pop();
-  return activities;
-}
+// const hobbyData = JSON.parse(localStorage.getItem('data'));
+//
+// const shuffleArray = arr => arr.sort(() => Math.random() - 0.5)
+//
+// console.log(shuffleArray(hobbyData));
+//
+// var columnDiv = document.getElementsByClassName('column');
+//
+// function createActivityList(hobbyArray, time) {
+//   let totalTime = 0;
+//   let activities = [];
+//   for (var i = 0; i < hobbyArray.length; i++) {
+//     if (totalTime <= time) {
+//       totalTime += hobbyArray[i].minTime;
+//       activities.push(hobbyArray[i]);
+//     } else {
+//       break;
+//     }
+//   }
+//   activities.pop();
+//   return activities;
+// }
+//
+// function showActivities() {
+//   let activityList = createActivityList(hobbyData, time);
+//   var newSection = document.createElement("section");
+//   var newContent = document.createTextNode('hi');
+//   newSection.appendChild(newContent);
+//   var currentDiv = document.getElementById("div1");
+//   document.body.insertBefore(newSection, currentDiv);
+// }
